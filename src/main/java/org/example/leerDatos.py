0bb4ff1f0763ffe139f9706b94d5b38d217bf115
JuanCarlos92 <juancarlosfilter@gmail.com>
2024-11-07 17:14:23 +0100

    ini repo

---
+++ src/main/java/org/example/leerDatos.py
@@ -0,0 +1,6 @@
+from sys import stdin
+
+mensaje =[mensaje for mensaje in stdin]
+
+for index, mensaje in enumerate(mensajes);
+    print(f'{index}. {mensaje}' end="")
---
+++ src/main/java/org/example/pipes_escribir.java
@@ -0,0 +1,9 @@
+package U3_Socket.Tuberias;
+
+public class pipes_escribir {
+    public static void main(String[] args) {
+        for (String mensaje : args){
+            System.out.println(mensaje);
+        }
+    }
+}
